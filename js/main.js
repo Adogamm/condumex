e1de6f0d90b4258dc2ba177f3d13139cf60b8146
Adogamm <1718110396@utectulancingo.edu.mx>
2021-06-21 15:48:28 -0500

    Actualización script SQL y graficas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+var graficabarrascontexto=document.getElementById("grafica-barras");
+var graficabarras=new Chart(graficabarrascontexto,{
+    type:"bar",
+    data:{
+        labels:["1ro","2do","3ro","4to"],
+        datasets:[{
+            label: "Rendimiento por turno",
+            data:[7,8,10,9],
+            backgroundColor:[
+                'rgb(66, 134, 244, 0.5)',
+                'rgb(74, 135, 72, 0.5)',
+                'rgb(229, 80, 72, 0.5)',
+                'rgb(10, 180, 172, 0.5)',
+            ]
+        }]
+    }
+});
 
+var graficalineascontexto=document.getElementById("grafica-lineas");
+var graficalineas=new Chart(graficalineascontexto,{
+    type:"line",
+    data:{
+      labels:["1ro","2do","3ro","4to"],
+      datasets:[{
+      label: "Velocidad de la maquína",
+      data:[7,8,10,9],
+      backgroundColor:[
+        'rgb(66, 134, 244, 0.5)',
+        'rgb(74, 135, 72, 0.5)',
+        'rgb(229, 80, 72, 0.5)',
+        'rgb(10, 180, 172, 0.5)',
+      ]
+    }]
+  }
+});

ad8ae57b3a019799b5d1c4c66565c196fe716553
Adogamm <1718110396@utectulancingo.edu.mx>
2021-06-22 11:28:42 -0500

    crear usuario para la bdd, crear conexion a la bdd

---
+++ databaseconnect/conection.php
@@ -0,0 +1,8 @@
+<?php
+$conexion = mysqli_connect("localhost","PRUEBAUSER","Pruebas1","PRUEBAS");
+// if (!$conexion) {
+//    echo 'Error al conectar a la base de datos';
+// } else {
+//     echo 'Conectado a la base de datos';
+// }
+
